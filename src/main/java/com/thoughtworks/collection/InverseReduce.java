b3d6c98b9f2ec5ca52282ecc367d510ec8888714
Chaowei Zeng <sli616@163.com>
2019-07-15 16:05:00 +0800

    finish inverseReduce

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -1,21 +1,40 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
+
+import static java.util.Collections.emptyList;
 
 public class InverseReduce {
 
     private Random random = new Random();
 
-    public InverseReduce() {}
+    public InverseReduce() {
+    }
 
     public InverseReduce(Random random) {
         this.random = random;
     }
 
-    public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+    public List<Integer> divideToSmaller(int inputNumber) {
+        int stepSize = random.nextInt(3);
+
+        if (stepSize > inputNumber || stepSize == 0) {
+            return emptyList();
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int dividedBoarder = inputNumber / stepSize;
+        IntStream.rangeClosed(1, dividedBoarder)
+                .boxed()
+                .reduce(inputNumber, (a, b) -> {
+                    int next = inputNumber - stepSize * b;
+                    result.add(next);
+                    return next;
+                });
+
+        return result;
     }
 }
